558140f7f08df306a9e914b3bfbb8c3a371c70c1
ananc <ana.nobre@montesclaros.mg.gov.br>
2026-08-13 17:32:32 -0300

    initial commit

---
+++ src/test/java/com/analuizanc/atendimento/AtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analuizanc.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
