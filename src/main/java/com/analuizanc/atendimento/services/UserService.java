bac71b5443dff1c0618facf8559ab8e90c2d955f
AnaLuizanc <analuizanc13@gmail.com>
2026-08-21 18:40:12 -0300

    feat: integrate Spring Security with password encryption and basic security config

--- src/main/java/com/analuizanc/atendimento/services/UserService.java
+++ src/main/java/com/analuizanc/atendimento/services/UserService.java
@@ -4,6 +4,7 @@ import com.analuizanc.atendimento.entities.User;
 import com.analuizanc.atendimento.entities.dtos.UserRequestDto;
 import com.analuizanc.atendimento.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public List<User> findAll() {
         return userRepository.findAll();
@@ -19,9 +21,13 @@ public class UserService {
 
     public User save(UserRequestDto data) {
         User user = new User();
+
         user.setName(data.name());
         user.setEmail(data.email());
-        user.setPassword(data.password());
+
+        String encryptedPassword = passwordEncoder.encode(data.password());
+        user.setPassword(encryptedPassword);
+
         user.setRole(data.role());
 
         return userRepository.save(user);
